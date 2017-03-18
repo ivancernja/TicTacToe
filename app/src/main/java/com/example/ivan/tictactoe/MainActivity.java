fc5f9153ca2040aea2a62236c816db7df4e640e0
ivancernja <ivancernja@gmail.com>
2017-03-18 19:59:03 +0100

    grid adjusted, objects added

--- app/src/main/java/com/example/ivan/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ivan/tictactoe/MainActivity.java
@@ -2,8 +2,23 @@ package com.example.ivan.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.o);
+
+        counter.animate().translationYBy(1000f).rotation(300).setDuration(300);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
